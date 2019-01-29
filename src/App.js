1705ba52bb5742cfc5b00f1e9a01ec8bd089b46b
Alvin Allen <alaj6625@gmail.com>
2019-01-29 16:18:50 -0600

    Added 1 row of buttons

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
+import "./App.css";
+import Button from "./components/Button";
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {  }
   }
   render() { 
-    return <div className="app">
+    return (<div className="app">
       <div className="calc-wrapper">
-        test
-      </div>   
-    </div>;
+      <div className="row">
+        </div>
+
+        <Button>7</Button>
+        <Button>8</Button>
+        <Button>9</Button>
+        <Button>0</Button>
+      </div>
+    </div>
+    );
   }
 }
  
